6d142586c9fc8bd693ae2f566c489c492d16387a
Dmitriy <bigtroble@gmail.com>
2020-02-06 14:30:10 +0300

    Update UmiVariables.php

--- src/UmiVariables.php
+++ src/UmiVariables.php
@@ -2,9 +2,21 @@
 namespace DgroupsVariables;
 
 class UmiVariables{
+
   public function defineVars($variables)
   {
-    var_dump($variables);
-    exit();
+      define("METHOD",getArrayKey($variables,'@method'));
+      define("MODULE",getArrayKey($variables,'@module'));
+      define("DOMAIN",getArrayKey($variables,'@domain'));
+      define("PAGE_ID",getArrayKey($variables,'@pageId'));
+      define("IS_DEFAULT_PAGE",getArrayKey($variables,'@is-default'));
+      define("REQUEST_URI",getArrayKey($variables,'@request-uri'));
+
+      $page = getArrayKey($variables,'full:page');
+      $parents = getArrayKey($variables,'parents');
+      $parents = getArrayKey($variables,'+page');
+      
+      define("PAGE_ID",$page instanceof umiHierarchyElement ? $page->getId() : false);
+      
   }
 }
